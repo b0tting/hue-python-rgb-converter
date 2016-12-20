e8921fd4d4adcc0b242e2292291e54660b4caec0
b0tting <otster@hotmail.com>
2016-12-20 16:38:12 +0100

    Restructured

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(name='hue-python-rgb-converter',
     description='RGB conversion tool written in Python for Philips Hue',
     author='Benjamin Knight',
     version="1.0",
-    package-dir={'': 'rgb_xy'},
     packages=['rgb_xy'],
     install_requires=[],
 )
