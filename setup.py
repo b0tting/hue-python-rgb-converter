39b99d661a282c887cb14ef80dc37675d5c64c06
b0tting <otster@hotmail.com>
2016-12-20 15:46:18 +0100

    Added setup.py so we can use this tool in pip and a requirements.txt file

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='hue-python-rgb-converter',
+    description='RGB conversion tool written in Python for Philips Hue',
+    author='Benjamin Knight',
+    version="1.0",
+    packages=find_packages(),
+    include_package_data=False,
+    install_requires=[],
+)
